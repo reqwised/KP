f4d94d2dcd74465aa66d93295c7ebfc1709e15d1
Daniel Ritz <d4r1tz@gmail.com>
2023-08-08 15:19:58 +0700

    Update layout.php

--- layout.php
+++ layout.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['loggedin'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>DISNAV SMG</title>
+        <link rel = "icon" href = "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png" type = "image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
